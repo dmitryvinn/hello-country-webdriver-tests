b3444962d96b30d2b8723d5b6841aedd270b2b3c
Dmitry Vinnik <dmitryvinn@gmail.com>
2019-05-13 23:05:01 -0500

    Updating location of the apk

--- src/main/java/com/dvinnik/hellocountry/utils/Constants.java
+++ src/main/java/com/dvinnik/hellocountry/utils/Constants.java
@@ -2,7 +2,7 @@ package com.dvinnik.hellocountry.utils;
 
 public class Constants {
     public static final String WEBSITE_URL = "https://hello-country-react.appspot.com/";
-    public static final String APK_PATH = "./hello-country-selenium-test/app-release-signed.apk";
+    public static final String APK_PATH = System.getProperty("user.dir") + "/app-release-signed.apk";
     public static final String API_KEY = "";
     public static final String DRIVER_PATH = "./src/main/resources/operadriver";
 
